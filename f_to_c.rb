6f3c26404321df8797e93e05041196cad27d4119
Robert Allen <rfelixallen@gmail.com>
2015-01-19 15:36:47 -0500

    added last line for clarity

--- f_to_c.rb
+++ f_to_c.rb
@@ -3,7 +3,7 @@ def fahrenheit_to_celsius
 	puts "Whats the temperature? Please enter integer:\n"
 	fahrenheit = gets.chomp.to_f
 	celsius = ((fahrenheit - 32.0) * (5.0/9.0))
-	puts celsius
+	puts "The temperature in Celsius is: #{celsius} Degrees"
 end
 
 fahrenheit_to_celsius
