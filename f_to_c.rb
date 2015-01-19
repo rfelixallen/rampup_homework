bc3f87faedb60de19d9e624d183068570ee3a573
Robert Allen <rfelixallen@gmail.com>
2015-01-19 15:33:10 -0500

    function works

--- f_to_c.rb
+++ f_to_c.rb
@@ -1,6 +1,9 @@
 # convert fahrenheit to celsius.
 def fahrenheit_to_celsius
-	puts "Hello World"
+	puts "Whats the temperature? Please enter integer:\n"
+	fahrenheit = gets.chomp.to_f
+	celsius = ((fahrenheit - 32.0) * (5.0/9.0))
+	puts celsius
 end
 
 fahrenheit_to_celsius
