c37b269fa7d33602ec846d3f41c6a108c62e6e41
Robert Allen <rfelixallen@gmail.com>
2015-01-25 12:42:18 -0500

    added a second animal for demo

--- classes.rb
+++ classes.rb
@@ -1,4 +1,8 @@
 class Animal
+	def set_noise
+		@noise = "Moo!"
+	end
+
 	def make_noise
 		#"Moo!"
 		@noise
@@ -6,4 +10,8 @@ class Animal
 end
 
 cow = Animal.new
+cow.set_noise
 puts cow.make_noise
+
+horse = Animal.new
+puts horse.make_noise
