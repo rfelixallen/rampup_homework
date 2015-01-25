140f7c3d523e8ccce302b804413c8a51d6072ba7
Robert Allen <rfelixallen@gmail.com>
2015-01-25 13:37:18 -0500

    experimenting with accessor writer and reader and init

--- classes.rb
+++ classes.rb
@@ -2,10 +2,18 @@
 # Animal will set a noise and get a noise.
 # Set a noise requires user input
 class Animal
+	attr_accessor :name
+	attr_writer :color
+	attr_reader :legs, :arms
+
+	#attr_writer :noise
+# attr_writer is the same as the following code
 	def noise=(noise)
 		@noise = noise
 	end
 
+	#attr_reader :noise
+# attr_reader is the same as the following code
 	def noise
 		#"Moo!"
 		@noise
