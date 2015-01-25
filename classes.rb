ae4e72eb445264cd6c7846d2673b497b21728b6c
Robert Allen <rfelixallen@gmail.com>
2015-01-25 13:20:28 -0500

    fixed program

--- classes.rb
+++ classes.rb
@@ -2,20 +2,20 @@
 # Animal will set a noise and get a noise.
 # Set a noise requires user input
 class Animal
-	def set_noise(noise)
+	def noise=(noise)
 		@noise = noise
 	end
 
-	def get_noise
+	def noise
 		#"Moo!"
 		@noise
 	end
 end
 
 cow = Animal.new
-cow.set_noise("Moo!")
-puts cow.get_noise
+cow.noise = "Moo!"
+puts cow.noise
 
 horse = Animal.new
-horse.set_noise("Neigh!")
-puts horse.get_noise
+horse.noise = "Neigh!"
+puts horse.noise
