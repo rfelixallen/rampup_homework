a7fbc8b02a9ca8d31baae9cb676026cf023d7655
Robert Allen <rfelixallen@gmail.com>
2015-01-25 19:37:56 -0500

    Can use modules and have classes inherit infor from modules, and then other classes

--- person.rb
+++ person.rb
@@ -1,4 +1,4 @@
-class Person
+module ContactInfo
 	attr_accessor :first_name, :last_name, :city, :state, :zip_code
 
 	def full_name
@@ -11,4 +11,16 @@ class Person
 		csz += ", #{@zip_code}" if @zip_code
 		return csz
 	end
+end
+
+class Person
+	include ContactInfo
+end
+
+class Teacher < Person
+	attr_accessor :lesson_plans
+end
+
+class Student < Person
+	attr_accessor :books, :grades
 end
